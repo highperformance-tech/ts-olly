f9c88b9973cc2fe78f9b2a6a0ea6dd9a94b6d6f1
Brad Fair <brad.fair@highperformance.tech>
2026-01-07 15:16:09 -0600

    fix(server): use standard /metrics path for Prometheus endpoint
    Fixes #2

--- cmd/ts-olly/routes.go
+++ cmd/ts-olly/routes.go
@@ -7,7 +7,7 @@ import (
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/metrics", func(w http.ResponseWriter, req *http.Request) {
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request) {
 		metrics.WritePrometheus(w, true)
 	})
 	return mux
